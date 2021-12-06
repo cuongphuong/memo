6b1147d5e0b17ebfd4b905f85dcba204ffcc5e7f
BuiDucNam <namkhang714@gmail.com>
2021-12-06 21:39:17 +0700

    mm: update search function

--- src/LayoutComponents/QuickSearchTab.js
+++ src/LayoutComponents/QuickSearchTab.js
@@ -4,35 +4,75 @@ import Viewer from '../ViewComponents/Viewer';
 import { ContentRender } from '../Utils/ContentRender';
 import List from './List';
 
+//remove the sign
+function removeAccents(str) {
+    return str.normalize('NFD')
+        .replace(/[\u0300-\u036f]/g, '')
+        .replace(/đ/g, 'd').replace(/Đ/g, 'D');
+}
+
 export default function QuickSearchTab() {
 
     const [mdContent, setMdContent] = useState("No content result ...");
-    const [resultList, setResultList] = useState([]);
+    // const [resultList, setResultList] = useState([]);
+    const [resultList, setResultList] = useState({});
     const [activeId, setActiveId] = useState("");
-    const inputObj = useRef(null);
+    const [searchKey, setSearchKey] = useState("");
+    const prevSearchKey = useRef(null);
+    const [countKeyword, setCountKeyword] = useState([]);
+
+    // const inputObj = useRef(null);
 
     let typingTimer;                // timer identifier
-    let doneTypingInterval = 1000;  // time in ms (600ms)
+    let doneTypingInterval = 600;  // time in ms (600ms)
 
+    // useEffect(() => {
+    //     inputObj.current.focus();
+    // }, [])
+
+    // function handleSearchChange(evt) {
+    //     let searchKeyWord = evt.target.value;
+    //     setResultList([]);
+    //     clearTimeout(typingTimer);
+    //     if (searchKeyWord) {
+    //         typingTimer = setTimeout(function () {
+    //             doneTyping(evt.target.value)
+    //         }, doneTypingInterval);
+    //     }
+    // }
     useEffect(() => {
-        inputObj.current.focus();
-    }, [])
+        let searchContent = localStorage.getItem("searchContent");
+        if (searchContent) {
+            let contents = JSON.parse(searchContent);
+            setResultList(contents);
+        }
+    }, [searchKey])
 
     function handleSearchChange(evt) {
         let searchKeyWord = evt.target.value;
-        setResultList([]);
-        clearTimeout(typingTimer);
-        if (searchKeyWord) {
-            typingTimer = setTimeout(function () {
-                doneTyping(evt.target.value)
-            }, doneTypingInterval);
+        setSearchKey(searchKeyWord);
+
+        if (prevSearchKey.current) {
+            clearTimeout(prevSearchKey.current)
         }
+
+        prevSearchKey.current = setTimeout(() => {
+            doneTyping(searchKeyWord);
+        }, doneTypingInterval)
     }
 
     async function doneTyping(keyword) {
         let apiResultContentList = await ContentRender.search(keyword);
+
         if (apiResultContentList && apiResultContentList.length > 0) {
-            setResultList(apiResultContentList);
+            const key = { [keyword]: [...apiResultContentList], ...resultList };
+            setResultList(key);
+            localStorage.setItem("searchContent", JSON.stringify(key));
+
+            //countKeyword
+            let count = [...countKeyword];
+            count.push(keyword);
+            setCountKeyword(count);
         }
     }
 
@@ -44,11 +84,13 @@ export default function QuickSearchTab() {
     return (
         <>
             <Layout.SiderBar>
-                <input ref={inputObj} onChange={(evt) => { handleSearchChange(evt) }} type="text" id="pg_mm_search_input" placeholder="Type for search..." />
+                {/* <input ref={inputObj} onChange={(evt) => { handleSearchChange(evt) }} type="text" id="pg_mm_search_input" placeholder="Type for search..." /> */}
+                <input value={searchKey} onChange={(evt) => { handleSearchChange(evt) }} type="text" id="pg_mm_search_input" placeholder="Type for search..." />
                 <hr />
                 <List>
-                    {resultList.map((item, index) => <List.Item activeId={activeId} handleChooseItem={handleChooseItem} key={index} source={item} />)}
+                    {resultList[searchKey]?.map((item, index) => <List.Item activeId={activeId} handleChooseItem={handleChooseItem} key={index} source={item} />)}
                 </List>
+
             </Layout.SiderBar>
             <Layout.Content>
                 <Viewer source={mdContent} />
